f5fddd14a84597456c3fbdef79c6b98f4802bef9
Jaya Krishna Gaddam <2100090162@kluniversity.in>
2024-02-28 08:48:31 +0530

    Backed Up

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, flash, request, redirect, jsonify
 from flask_pymongo import PyMongo
-from classify_image import train_model, predict_image
 import os
 
 app = Flask(__name__)
@@ -8,38 +7,12 @@ app.config["SECRET_KEY"] = "1234"  # Add a secret key for flash messages
 app.config["MONGO_URI"] = "mongodb+srv://2100090162:manigaddam@deepsheild.kzgpo9p.mongodb.net/deepsheild"
 mongo = PyMongo(app)
 
-real_folder = "Real"
-fake_folder = "Fake"
-
-# Train the model
-train_model(real_folder, fake_folder)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-
-@app.route('/classify_image', methods=['POST'])
-def classify_image():
-    # Check if the request has the file part
-    if 'image' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['image']
-    # If the user does not select a file, the browser submits an empty file without a filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file:
-        # Save the file to a temporary location
-        image_path = "temp_image.jpg"
-        file.save(image_path)
-        # Perform image classification
-        result = predict_image(image_path)
-        # Render a template with the classification result
-        return render_template('classification_result.html', result=result)
 
 
 @app.route('/home')
@@ -99,19 +72,9 @@ def videodrop():
     return render_template('videodrop.html')
 
 
-@app.route('/imagedrop', methods=['POST'])
+@app.route('/imagedrop')
 def imagedrop():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image file provided'})
-
-    image_file = request.files['image']
-
-    # Perform image classification
-    prediction = predict_image(image_file)
-
-    # Return the classification result
-    return jsonify({'prediction': prediction})
-
+    return render_template('imagedrop.html')
 
 @app.route('/audiodrop')
 def audiodrop():
