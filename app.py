6d14bb4d580a7cf95166455c73a2aa82583552eb
jayakrishnagaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2024-03-21 23:32:34 +0530

    Successfully predicting an Image...

--- app.py
+++ app.py
@@ -1,16 +1,81 @@
 from flask import Flask, render_template, url_for, flash, request, redirect, jsonify, session
 from flask_pymongo import PyMongo
 import os
+import torch
+import torch.nn.functional as F
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import zipfile
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1234"  # Add a secret key for flash messages
 app.config["MONGO_URI"] = "mongodb+srv://2100090162:manigaddam@deepsheild.kzgpo9p.mongodb.net/deepsheild"
 mongo = PyMongo(app)
 
+with zipfile.ZipFile("examples.zip", "r") as zip_ref:
+    zip_ref.extractall(".")
+
+DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+mtcnn = MTCNN(
+    select_largest=False,
+    post_process=False,
+    device=DEVICE
+).eval()
+
+model = InceptionResnetV1(
+    pretrained="vggface2",
+    classify=True,
+    num_classes=1,
+    device=DEVICE
+)
+
+checkpoint = torch.load("resnetinceptionv1_epoch_32.pth", map_location=torch.device('cpu'))
+model.load_state_dict(checkpoint['model_state_dict'])
+model.to(DEVICE)
+model.eval()
+
+def predict(input_image_path):
+    """Predict whether the image contains a real or fake face"""
+    image = cv2.imread(input_image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    
+    face = mtcnn(image)
+    if face is None:
+        return "No face detected"
+    
+    face = face.unsqueeze(0)
+    face = F.interpolate(face, size=(256, 256), mode='bilinear', align_corners=False)
+    face = face.to(DEVICE, dtype=torch.float32) / 255.0
+    
+    with torch.no_grad():
+        output = torch.sigmoid(model(face).squeeze(0))
+        prediction = "Real" if output.item() < 0.5 else "Fake"
+
+    return prediction
 #added this
 @app.route('/')
 def index(): 
     return render_template('index.html')
+
+@app.route('/prediction/<prediction>')
+def prediction(prediction):
+    return render_template('prediction.html',prediction=prediction)
+
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    if 'image' in request.files:
+        image = request.files['image']
+        image_path = "temp_image.jpg"
+        image.save(image_path)
+        prediction = predict(image_path)
+        return redirect(url_for('prediction', prediction=prediction))
+    else:
+        return "No image found in request!"
 
 
 @app.route('/logout')
