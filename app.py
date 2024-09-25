2c6fb3805ec055982aa2ea36936e1daecfdfc2f7
jayakrishnagaddam <2100090162@kluniversity.in>
2024-09-25 16:07:56 +0530

    Added Google API and Login

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for, flash, request, redirect, session
+from flask import Flask, render_template, url_for, flash, request, redirect, session, abort
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+import google.auth.transport.requests
 from flask_pymongo import PyMongo
 import os
+import pathlib
+import requests
+from pip._vendor import cachecontrol
 import torch
 import torch.nn.functional as F
 from facenet_pytorch import MTCNN, InceptionResnetV1
@@ -12,9 +18,22 @@ import base64
 from io import BytesIO
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "1234"
+app.config["SECRET_KEY"] = "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"
 app.config["MONGO_URI"] = "mongodb+srv://2100090162:manigaddam@deepsheild.kzgpo9p.mongodb.net/deepsheild"
 mongo = PyMongo(app)
+
+GOOGLE_CLIENT_ID = "327025315574-urqtgpl60f11h2gv436pbajok3q0vtg9.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "API.json")
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://localhost:5000/callback"
+)
+
 
 with zipfile.ZipFile("examples.zip", "r") as zip_ref:
     zip_ref.extractall(".")
@@ -41,54 +60,48 @@ model.eval()
 
 
 def predict(input_image_path):
-    """Predict whether the image contains a real or fake face"""
+    
     image = cv2.imread(input_image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = Image.fromarray(image)
-    
+
     face = mtcnn(image)
     if face is None:
         return "No face detected", None, None, None
-    
-    # Face detection
+
     face_bboxes, _ = mtcnn.detect(image)
-    
+
     heatmap = np.zeros_like(image)
-    
+
     for bbox in face_bboxes:
         x, y, w, h = bbox.astype(int)
-        heatmap[y:y+h, x:x+w] = 1
-        
-    # Normalize heatmap
+        heatmap[y:y + h, x:x + w] = 1
+
     heatmap = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min())
-    
-    # Convert to RGB
+
     heatmap = cv2.applyColorMap(np.uint8(255 * heatmap), cv2.COLORMAP_JET)
     heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
-    
-    # Overlay heatmap on the original image
+
     overlayed_image = cv2.addWeighted(np.array(image), 0.7, np.array(heatmap), 0.3, 0, dtype=cv2.CV_8U)
-    
-    # Convert overlayed image to PIL format
+
     overlayed_image_pil = Image.fromarray(overlayed_image)
-    
-    # Face classification
+
+ 
     face = F.interpolate(torch.tensor(face).unsqueeze(0), size=(256, 256), mode='bilinear', align_corners=False)
     face = face.to(DEVICE, dtype=torch.float32) / 255.0
-    
+
     with torch.no_grad():
         output = torch.sigmoid(model(face).squeeze(0))
         prediction = "Real" if output.item() < 0.5 else "Fake"
-        
-    # Convert prediction and overlayed image to base64 for passing to HTML
+
     prediction_base64 = image_to_base64(overlayed_image_pil)
-    
-    # Additional information
+
     original_img_base64 = image_to_base64(image)
     metadata = {'date': '2024-03-21', 'time': '12:00', 'location': 'Unknown'}
     probability_score = output.item()
-    
+
     return prediction, prediction_base64, original_img_base64, metadata, probability_score
+
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
@@ -103,20 +116,21 @@ def process_image():
 
 
 def image_to_base64(image):
-    """Converts PIL image to base64 format for displaying in HTML"""
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
     return img_str
 
+
 @app.route('/')
-def index(): 
+def index():
     return render_template('index.html')
+
 
 @app.route('/logout')
 def logout():
-    session.pop('email', None)  
-    flash('You have been logged out', 'success') 
+    session.pop('email', None)
+    flash('You have been logged out', 'success')
     return redirect(url_for('login'))
 
 
@@ -129,12 +143,14 @@ def home():
         return redirect(url_for('login'))
 
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-
-    if request.method == 'POST':
+    if 'google_login' in request.form:
+        authorization_url, state = flow.authorization_url()
+        session["state"] = state
+        return redirect(authorization_url)
+    if request.method == 'POST' and 'manual_login' in request.form:
         email = request.form.get('email')
         password = request.form.get('password')
 
@@ -143,13 +159,34 @@ def login():
         if user_data:
             firstname = user_data['first_name']
             session['email'] = email
-            session['name']=firstname
+            session['name'] = firstname
             return redirect(url_for('home'))
         else:
             error = 'Invalid username or password'
 
     return render_template('login.html', error=error)
 
+@app.route('/callback')
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+    
+   
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    session["email"] = id_info.get("email")
+
+    return redirect(url_for('home'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -163,8 +200,7 @@ def signup():
 
         mongo.db.users.insert_one(user_data)
 
-        flash('SIGN UP SUCCESSFUL...YOU CAN NOW LOGIN HERE...',
-              'success')  # Flash success message
+        flash('SIGN UP SUCCESSFUL...YOU CAN NOW LOGIN HERE...', 'success')
         return redirect(url_for('login'))
     return render_template('signup.html')
 
@@ -187,6 +223,7 @@ def videodrop():
 @app.route('/imagedrop')
 def imagedrop():
     return render_template('imagedrop.html')
+
 
 @app.route('/audiodrop')
 def audiodrop():
